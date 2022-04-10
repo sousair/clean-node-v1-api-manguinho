4cc1f1bd9f468c36746e8f60c44ec6f7fc6dfaa5
Iago Rocha de Sousa <sousair2001@gmail.com>
2022-04-10 18:40:41 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/database/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,36 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository', () => {
+  const accountData = {
+    name: 'validName',
+    email: 'validEmail@domain.com',
+    password: 'validHashedPassword',
+  };
+
+  let mongoHelper: MongoHelper;
+  let sut: AccountMongoRepository;
+
+  beforeAll(async () => {
+    mongoHelper = new MongoHelper();
+    await mongoHelper.connect(process.env.MONGO_URL || '');
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+
+  beforeEach(() => {
+    sut = new AccountMongoRepository(mongoHelper);
+  });
+
+  test('Should return an account on success', async () => {
+    const account = await sut.add(accountData);
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe(accountData.name);
+    expect(account.email).toBe(accountData.email);
+    expect(account.password).toBe(accountData.password);
+  });
+});
