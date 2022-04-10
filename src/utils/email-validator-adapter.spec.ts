185c5a122e02a7a8db266cf6d2fd19e9d1815007
Iago Rocha de Sousa <sousair2001@gmail.com>
2022-04-10 00:15:53 -0300

    fix: change email-validator file name to match pattern

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,5 @@
 import { EmailValidator } from '../presentation/protocols/email-validator';
-import { EmailValidatorAdapter } from './email-validator';
+import { EmailValidatorAdapter } from './email-validator-adapter';
 import validator from 'validator';
 
 describe('EmailValidator Adapter', () => {
